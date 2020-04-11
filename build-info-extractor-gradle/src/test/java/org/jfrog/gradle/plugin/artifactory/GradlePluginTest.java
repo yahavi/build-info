c6a4ab193728f33b94bf71adb515f485c3a7821e
yahavi <yahavi@jfrog.com>
2020-04-11 14:00:13 +0300

    Gradle tests

--- build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/GradlePluginTest.java
+++ build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/GradlePluginTest.java
@@ -3,12 +3,12 @@ package org.jfrog.gradle.plugin.artifactory;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.util.VersionNumber;
 import org.jfrog.build.IntegrationTestsBase;
+import org.jfrog.build.api.BuildInfoConfigProperties;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,13 +24,20 @@ import static org.testng.FileAssert.fail;
 @Test
 public class GradlePluginTest extends IntegrationTestsBase {
 
+    // Repositories
     private static final String GRADLE_LOCAL_REPO = "build-info-tests-gradle-local";
     private static final String GRADLE_REMOTE_REPO = "build-info-tests-gradle-remote";
     private static final String GRADLE_VIRTUAL_REPO = "build-info-tests-gradle-virtual";
 
-    private static final Path PROJECTS_ROOT = Paths.get(".").normalize().toAbsolutePath().resolve(Paths.get("src", "test", "resources", "org", "jfrog", "gradle", "plugin", "artifactory"));
-    private static final File GRADLE_EXAMPLE_DIR = PROJECTS_ROOT.resolve("gradle-example").toFile();
-    private static final File GRADLE_EXAMPLE_PUBLISH_DIR = PROJECTS_ROOT.resolve("gradle-example-publish").toFile();
+    // Root directories
+    static final Path GRADLE_EXTRACTOR = Paths.get(".").normalize().toAbsolutePath();
+    static final Path GRADLE_EXTRACTOR_SRC = GRADLE_EXTRACTOR.resolve("src");
+    static final Path PROJECTS_ROOT = GRADLE_EXTRACTOR_SRC.resolve(Paths.get("test", "resources", "integration"));
+
+    // Projects
+    private static final Path GRADLE_EXAMPLE = PROJECTS_ROOT.resolve("gradle-example");
+    private static final Path GRADLE_EXAMPLE_PUBLISH = PROJECTS_ROOT.resolve("gradle-example-publish");
+    private static final Path GRADLE_EXAMPLE_CI_SERVER = PROJECTS_ROOT.resolve("gradle-example-ci-server");
 
     private Map<String, String> envVars;
 
@@ -61,10 +68,21 @@ public class GradlePluginTest extends IntegrationTestsBase {
     }
 
     @Test(dataProvider = "gradleVersions")
+    public void configurationsTest(String gradleVersion) {
+        try {
+            createTestDir(GRADLE_EXAMPLE);
+            BuildResult buildResult = runGradle(gradleVersion, envVars, false);
+            checkBuildResults(buildResult, false, dependenciesClient, getUrl(), localRepo);
+        } catch (IOException e) {
+            fail(e.getMessage(), e);
+        }
+    }
+
+    @Test(dataProvider = "gradleVersions")
     public void publicationsTest(String gradleVersion) {
         try {
-            createTestDir(GRADLE_EXAMPLE_PUBLISH_DIR);
-            BuildResult buildResult = runGradle(gradleVersion, envVars);
+            createTestDir(GRADLE_EXAMPLE_PUBLISH);
+            BuildResult buildResult = runGradle(gradleVersion, envVars, false);
             checkBuildResults(buildResult, VersionNumber.parse(gradleVersion).getMajor() >= 6, dependenciesClient, getUrl(), localRepo);
         } catch (IOException e) {
             fail(e.getMessage(), e);
@@ -72,11 +90,14 @@ public class GradlePluginTest extends IntegrationTestsBase {
     }
 
     @Test(dataProvider = "gradleVersions")
-    public void configurationsTest(String gradleVersion) {
+    public void ciServerTest(String gradleVersion) {
         try {
-            createTestDir(GRADLE_EXAMPLE_DIR);
-            BuildResult buildResult = runGradle(gradleVersion, envVars);
-            checkBuildResults(buildResult, false, dependenciesClient, getUrl(), localRepo);
+            createTestDir(GRADLE_EXAMPLE_CI_SERVER);
+            generateInitScript();
+            String propertiesFilePath = generateBuildInfoProperties(getUrl(), getUsername(), getPassword(), localRepo, virtualRepo);
+            envVars.put(BuildInfoConfigProperties.PROP_PROPS_FILE, propertiesFilePath);
+            BuildResult buildResult = runGradle(gradleVersion, envVars, true);
+            checkBuildResults(buildResult, VersionNumber.parse(gradleVersion).getMajor() >= 6, dependenciesClient, getUrl(), localRepo);
         } catch (IOException e) {
             fail(e.getMessage(), e);
         }
--- build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/Utils.java
+++ build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/Utils.java
@@ -8,6 +8,11 @@ import org.jfrog.build.extractor.clientConfiguration.client.ArtifactoryDependenc
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +27,15 @@ import static org.testng.Assert.assertNotNull;
  */
 public class Utils {
 
+    // The temporary test dir. We run the tests here.
     private static final File TEST_DIR = new File(System.getProperty("java.io.tmpdir"), "gradle_tests_space");
+
+    // CI example paths
+    static final Path LIBS_DIR = GradlePluginTest.GRADLE_EXTRACTOR.resolve(Paths.get("build", "libs"));
+    static final Path INIT_SCRIPT = GradlePluginTest.GRADLE_EXTRACTOR_SRC.resolve(Paths.get("main", "resources", "initscripttemplate.gradle"));
+    static final Path BUILD_INFO_PROPERTIES = GradlePluginTest.PROJECTS_ROOT.resolve("buildinfo.properties");
+
+    // Expected artifacts
     private static final String ARTIFACTS_GROUP_ID = "/org/jfrog/test/gradle/publish/";
     private static final String[] EXPECTED_ARTIFACTS = {
             "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.jar",
@@ -44,8 +57,11 @@ public class Utils {
                     "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.module")).
             toArray(String[]::new);
 
-    static BuildResult runGradle(String gradleVersion, Map<String, String> envVars) {
-        List<String> arguments = Arrays.asList("clean", "artifactoryPublish", "--stacktrace");
+    static BuildResult runGradle(String gradleVersion, Map<String, String> envVars, boolean applyInitScript) {
+        List<String> arguments = new ArrayList<>(Arrays.asList("clean", "artifactoryPublish", "--stacktrace"));
+        if (applyInitScript) {
+            arguments.add("--init-script=gradle.init");
+        }
         //noinspection UnstableApiUsage
         return GradleRunner.create()
                 .withGradleVersion(gradleVersion)
@@ -56,12 +72,32 @@ public class Utils {
                 .build();
     }
 
-    static void createTestDir(File sourceDir) throws IOException {
-        FileUtils.copyDirectory(sourceDir, TEST_DIR);
+    static void createTestDir(Path sourceDir) throws IOException {
+        FileUtils.copyDirectory(sourceDir.toFile(), TEST_DIR);
     }
 
     static void deleteTestDir() throws IOException {
         FileUtils.deleteDirectory(TEST_DIR);
+    }
+
+    static void generateInitScript() throws IOException {
+        String content = new String(Files.readAllBytes(INIT_SCRIPT), StandardCharsets.UTF_8);
+        content = content.replace("${pluginLibDir}", LIBS_DIR.toString());
+        Path target = TEST_DIR.toPath().resolve("gradle.init");
+        Files.write(target, content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @SuppressWarnings("RegExpRedundantEscape")
+    static String generateBuildInfoProperties(String contextUrl, String username, String password, String localRepo, String virtualRepo) throws IOException {
+        String content = new String(Files.readAllBytes(BUILD_INFO_PROPERTIES), StandardCharsets.UTF_8);
+        content = content.replaceAll("\\$\\{contextUrl\\}", contextUrl);
+        content = content.replaceAll("\\$\\{username\\}", username);
+        content = content.replaceAll("\\$\\{password\\}", password);
+        content = content.replaceAll("\\$\\{localRepo\\}", localRepo);
+        content = content.replaceAll("\\$\\{virtualRepo\\}", virtualRepo);
+        Path target = TEST_DIR.toPath().resolve("buildinfo.properties");
+        Files.write(target, content.getBytes(StandardCharsets.UTF_8));
+        return target.toString();
     }
 
     static void checkBuildResults(BuildResult buildResult, boolean expectModuleArtifacts, ArtifactoryDependenciesClient dependenciesClient, String url, String repo) throws IOException {
