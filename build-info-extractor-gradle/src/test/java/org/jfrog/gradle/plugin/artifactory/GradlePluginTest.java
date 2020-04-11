f5156e0e938d3ef6f687a5cfa46de25d579445ff
yahavi <yahavi@jfrog.com>
2020-04-11 10:41:49 +0300

    Gradle tests

--- build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/GradlePluginTest.java
+++ build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/GradlePluginTest.java
@@ -14,7 +14,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static org.jfrog.gradle.plugin.artifactory.Utils.*;
 import static org.testng.FileAssert.fail;
@@ -26,34 +25,18 @@ import static org.testng.FileAssert.fail;
 public class GradlePluginTest extends IntegrationTestsBase {
 
     private static final String GRADLE_LOCAL_REPO = "build-info-tests-gradle-local";
+    private static final String GRADLE_REMOTE_REPO = "build-info-tests-gradle-remote";
     private static final String GRADLE_VIRTUAL_REPO = "build-info-tests-gradle-virtual";
 
     private static final Path PROJECTS_ROOT = Paths.get(".").normalize().toAbsolutePath().resolve(Paths.get("src", "test", "resources", "org", "jfrog", "gradle", "plugin", "artifactory"));
     private static final File GRADLE_EXAMPLE_DIR = PROJECTS_ROOT.resolve("gradle-example").toFile();
     private static final File GRADLE_EXAMPLE_PUBLISH_DIR = PROJECTS_ROOT.resolve("gradle-example-publish").toFile();
-    private static final String ARTIFACTS_GROUP_ID = "/org/jfrog/test/gradle/publish/";
 
-    private static final String[] EXPECTED_ARTIFACTS = {
-            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.jar",
-            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.jar",
-            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.jar",
-            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.properties",
-            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.properties",
-            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.properties",
-            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.pom",
-            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.pom",
-            "api/ivy-1.0-SNAPSHOT.xml",
-            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.pom"
-    };
-
-    private static final String[] EXPECTED_MODULE_ARTIFACTS = Stream.concat(
-            Stream.of(EXPECTED_ARTIFACTS),
-            Stream.of("webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.module", "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.module", "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.module"))
-            .toArray(String[]::new);
     private Map<String, String> envVars;
 
     public GradlePluginTest() {
         localRepo = GRADLE_LOCAL_REPO;
+        remoteRepo = GRADLE_REMOTE_REPO;
         virtualRepo = GRADLE_VIRTUAL_REPO;
         envVars = new HashMap<String, String>() {{
             putAll(System.getenv());
@@ -61,6 +44,7 @@ public class GradlePluginTest extends IntegrationTestsBase {
             putIfAbsent(BITESTS_ARTIFACTORY_ENV_VAR_PREFIX + "USERNAME", getUsername());
             putIfAbsent(BITESTS_ARTIFACTORY_ENV_VAR_PREFIX + "PASSWORD", getPassword());
             putIfAbsent(BITESTS_ARTIFACTORY_ENV_VAR_PREFIX + "LOCAL_REPO", localRepo);
+            putIfAbsent(BITESTS_ARTIFACTORY_ENV_VAR_PREFIX + "VIRTUAL_REPO", virtualRepo);
         }};
     }
 
@@ -81,7 +65,7 @@ public class GradlePluginTest extends IntegrationTestsBase {
         try {
             createTestDir(GRADLE_EXAMPLE_PUBLISH_DIR);
             BuildResult buildResult = runGradle(gradleVersion, envVars);
-            checkBuildResults(buildResult, VersionNumber.parse(gradleVersion).getMajor() >= 6);
+            checkBuildResults(buildResult, VersionNumber.parse(gradleVersion).getMajor() >= 6, dependenciesClient, getUrl(), localRepo);
         } catch (IOException e) {
             fail(e.getMessage(), e);
         }
@@ -92,23 +76,9 @@ public class GradlePluginTest extends IntegrationTestsBase {
         try {
             createTestDir(GRADLE_EXAMPLE_DIR);
             BuildResult buildResult = runGradle(gradleVersion, envVars);
-            checkBuildResults(buildResult, false);
+            checkBuildResults(buildResult, false, dependenciesClient, getUrl(), localRepo);
         } catch (IOException e) {
             fail(e.getMessage(), e);
-        }
-    }
-
-    private void checkBuildResults(BuildResult buildResult, boolean expectModuleArtifacts) throws IOException {
-        // Assert all tasks ended with success outcome
-        assertSuccess(buildResult, ":api:artifactoryPublish");
-        assertSuccess(buildResult, ":shared:artifactoryPublish");
-        assertSuccess(buildResult, ":services:webservice:artifactoryPublish");
-        assertSuccess(buildResult, ":artifactoryPublish");
-
-        // Check that all expected artifacts uploaded to Artifactory
-        String[] expectedArtifacts = expectModuleArtifacts ? EXPECTED_MODULE_ARTIFACTS : EXPECTED_ARTIFACTS;
-        for (String expectedArtifact : expectedArtifacts) {
-            dependenciesClient.getArtifactMetadata(getUrl() + localRepo + ARTIFACTS_GROUP_ID + expectedArtifact);
         }
     }
 }
--- build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/Utils.java
+++ build-info-extractor-gradle/src/test/java/org/jfrog/gradle/plugin/artifactory/Utils.java
@@ -4,12 +4,14 @@ import org.apache.commons.io.FileUtils;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
+import org.jfrog.build.extractor.clientConfiguration.client.ArtifactoryDependenciesClient;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
 import static org.testng.Assert.assertEquals;
@@ -21,6 +23,26 @@ import static org.testng.Assert.assertNotNull;
 public class Utils {
 
     private static final File TEST_DIR = new File(System.getProperty("java.io.tmpdir"), "gradle_tests_space");
+    private static final String ARTIFACTS_GROUP_ID = "/org/jfrog/test/gradle/publish/";
+    private static final String[] EXPECTED_ARTIFACTS = {
+            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.jar",
+            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.jar",
+            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.jar",
+            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.properties",
+            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.properties",
+            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.properties",
+            "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.pom",
+            "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.pom",
+            "api/ivy-1.0-SNAPSHOT.xml",
+            "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.pom"
+    };
+    private static final String[] EXPECTED_MODULE_ARTIFACTS = Stream.concat(
+            Stream.of(EXPECTED_ARTIFACTS),
+            Stream.of(
+                    "webservice/1.0-SNAPSHOT/webservice-1.0-SNAPSHOT.module",
+                    "shared/1.0-SNAPSHOT/shared-1.0-SNAPSHOT.module",
+                    "api/1.0-SNAPSHOT/api-1.0-SNAPSHOT.module")).
+            toArray(String[]::new);
 
     static BuildResult runGradle(String gradleVersion, Map<String, String> envVars) {
         List<String> arguments = Arrays.asList("clean", "artifactoryPublish", "--stacktrace");
@@ -40,6 +62,20 @@ public class Utils {
 
     static void deleteTestDir() throws IOException {
         FileUtils.deleteDirectory(TEST_DIR);
+    }
+
+    static void checkBuildResults(BuildResult buildResult, boolean expectModuleArtifacts, ArtifactoryDependenciesClient dependenciesClient, String url, String repo) throws IOException {
+        // Assert all tasks ended with success outcome
+        assertSuccess(buildResult, ":api:artifactoryPublish");
+        assertSuccess(buildResult, ":shared:artifactoryPublish");
+        assertSuccess(buildResult, ":services:webservice:artifactoryPublish");
+        assertSuccess(buildResult, ":artifactoryPublish");
+
+        // Check that all expected artifacts uploaded to Artifactory
+        String[] expectedArtifacts = expectModuleArtifacts ? EXPECTED_MODULE_ARTIFACTS : EXPECTED_ARTIFACTS;
+        for (String expectedArtifact : expectedArtifacts) {
+            dependenciesClient.getArtifactMetadata(url + repo + ARTIFACTS_GROUP_ID + expectedArtifact);
+        }
     }
 
     static void assertSuccess(BuildResult buildResult, String taskName) {
